47fb3574cc72124bbec35911c760352a3fb995e9
Elena Yanushevskaya <helen.yanushevskaya@exness.com>
2023-05-22 20:29:53 +0300

    add new simple tests

--- test_main.py
+++ test_main.py
@@ -1,5 +1,11 @@
-def test_first_example():
-        assert 1 + 2 == 3
+import allure
 
+
+@allure.title('test_first_example')
+def test_first_example():
+    assert 1 + 2 == 3
+
+
+@allure.title('test_second_example')
 def test_second_example():
     assert 1 + 2 == 3
